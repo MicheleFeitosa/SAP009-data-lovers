9124c2a48677ac7c359a16d272243c15b6e804cd
michelefeitosa <michelefeitosa08@gmail.com>
2023-04-20 10:35:58 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,18 @@
+
+export const filtrarPorChampion = (arrayAllChampions,funcao) => {
+  return arrayAllChampions.filter((champion)=>champion.tags.includes(funcao));
+};
+
+
+export const ordenacaoAz = (arrayAllChampions) => {
+  return [
+    ...arrayAllChampions.map( x => ({...x}))].sort((a, b) => { //recriar um array pra ser ordenado 
+    return a.name < b.name ? -1 : 1; //if ternario
+  });
+};
+
+
+export const ordenacaoZa = (arrayAllChampions) => {
+  return ordenacaoAz(arrayAllChampions).reverse()
+}
+
---
+++ main.js
@@ -0,0 +1,46 @@
+
+import { filtrarPorChampion, ordenacaoAz, ordenacaoZa } from './data.js';
+import data from './data/lol/lol.js';
+
+const printarCards = document.getElementById('printarCards');
+const selectChampion = document.getElementById('selectChampion'); //pegar o select do HTML pelo id 
+const selectOrder = document.getElementById('selectOrder');
+const allChampions = data.data;
+const arrayAllChampions = Object.values(allChampions);
+
+criarCards(arrayAllChampions)
+selectChampion.addEventListener("change", ()=> {  
+  const funcao = selectChampion.value //pegou o valor que o usuario selecionou 
+  const arrayFiltrado = filtrarPorChampion(arrayAllChampions,funcao)
+  criarCards(arrayFiltrado); //criar uma função que pegue esse valor e print na tela
+  return funcao
+});
+
+selectOrder.addEventListener("change", ()=> {  
+  const ordenar = selectOrder.value 
+  let arrayFiltrado;
+  if (selectOrder.value === 'a-z') { 
+    arrayFiltrado = ordenacaoAz(arrayAllChampions,ordenar);
+  } else {
+    arrayFiltrado = ordenacaoZa(arrayAllChampions,ordenar); 
+  }
+  criarCards(arrayFiltrado); 
+  return ordenar
+});
+
+
+function criarCards(array){  //função que pega os dados e cria o card quando o usuario seleciona
+  printarCards.innerHTML = "" //limpar todo conteudo antes de aparecer os itens filtrados
+  for (const champion in array) {
+    const photo = array[champion].splash;
+    const name = array[champion].name;
+    const attack = array[champion].info.attack;
+    const defense = array[champion].info.defense;
+    const template=(`<section class="cards">
+    <img src = '${photo}'>
+    <div class='texto'><p>${name}</p><details><summary>info:</summary><p>Ataque:${attack}</p><p>Defesa:${defense}</p></details>
+    </div></section>`)
+    printarCards.innerHTML += template;
+  }
+  
+}
